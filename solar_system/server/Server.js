6a6355b1fc6aa9b212b59a22a6ad84642ee2089d
Leonard Mercedes <100613411+LMercedes03@users.noreply.github.com>
2023-06-13 17:32:37 -0400

    project server

---
+++ solar_system/server/Server.js
@@ -0,0 +1,62 @@
+const express = require('express')
+const request = require('request')
+const axios = require('axios')
+// const cors = require('cors')
+
+
+const PORT = 80
+const app = express()
+
+
+// Middleware...
+// app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+
+// Endpoints
+
+app.get('/planets', (req, res) => {
+  const name = req.query.name;
+  const options = {
+    url: 'https://api.api-ninjas.com/v1/planets?name=' + name,
+    headers: {
+      'X-Api-Key': 'GyVLRiMNYTbqEQmk6v2Fow==eo6gcx0ALMACZaQi'
+    }
+  };
+
+  request(options, (error, response, body) => {
+    if (error) {
+      console.error('Request failed:', error);
+      res.status(500).json({ message: 'Request failed' });
+    } else if (response.statusCode !== 200) {
+      console.error('Error:', response.statusCode, body.toString('utf8'));
+      res.status(response.statusCode).json({ message: 'Error' });
+    } else {
+      console.log(body);
+      res.json(JSON.parse(body));
+    }
+  });
+});
+
+
+app.get('/bodies/:id', async (req, res) => {
+  const { id } = req.params;
+  const url = `https://api.le-systeme-solaire.net/rest/bodies/${id}`;
+
+  try {
+    const response = await axios.get(url);
+    const data = response.data;
+    res.json(data);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ message: 'Error retrieving data' });
+  }
+});
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}...`);
+});
+
