cb54bf2dfc014523b8633bf29a2c25a844c790ea
Leonard Mercedes <100613411+LMercedes03@users.noreply.github.com>
2023-06-15 16:12:27 -0400

    Fix repo issue

--- solar_system/src/App.js
+++ solar_system/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Nav } from './Nav';
-import { Mercury } from './Pages/Mercury'
-import { Mars } from './Pages/Mars'
 import { Venus } from './Pages/Venus';
+import { Mercury } from './Pages/Mercury'
 import { Earth } from './Pages/Earth';
+import { Mars } from './Pages/Mars'
+import { Jupiter } from './Pages/Jupiter';
+import { Saturn } from './Pages/Saturn';
+import { Neptune } from './Pages/Neptune';
 import SolarSystem from './Pages/SolarSystem';
 import './App.css';
 
@@ -15,10 +18,13 @@ const App = () => {
         <Nav />
           <Routes>
             <Route path= "/" exact={true} element={<SolarSystem />}/>
-            <Route path= "/mars" exact={true} element={<Mars />}/>
-            <Route path= "/mercury" exact={true} element={<Mercury />}/>
             <Route path= "/venus" exact={true} element={<Venus />}/>
+            <Route path= "/mercury" exact={true} element={<Mercury />}/>
             <Route path= "/earth" exact={true} element={<Earth/>}/>
+            <Route path= "/mars" exact={true} element={<Mars />}/>
+            <Route path= "/jupiter" exact={true} element={<Jupiter />}/>
+            <Route path= "/saturn" exact={true} element={<Saturn />}/>
+            <Route path= "/neptune" exact={true} element={<Neptune />}/>
           </Routes> 
         </BrowserRouter> 
     </div>
--- solar_system/src/Nav.js
+++ solar_system/src/Nav.js
@@ -5,10 +5,14 @@ export const Nav = () => {
   return (
     <div>
       <Link to="/">Solar System</Link>{' '}
-      <Link to="/mars">Mars</Link>{' '}
-      <Link to="/mercury">Mercury</Link>{' '}
       <Link to="/venus">Venus</Link>{' '}
+      <Link to="/mercury">Mercury</Link>{' '}
       <Link to="/earth">Earth</Link>{' '}
+      <Link to="/mars">Mars</Link>{' '}
+      <Link to="/jupiter">Jupiter</Link>{' '}
+      <Link to="/saturn">Saturn</Link>{' '}
+      <Link to="/neptune">Neptune</Link>{' '}
+
     </div>
   )
 }
