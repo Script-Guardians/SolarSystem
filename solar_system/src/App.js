23f606140f0caa24de62d760cf57f472da5a2a16
Imane Allay <iallay@xpanxion.com>
2023-06-15 15:20:48 -0400

    Add Nav and App Pages

--- solar_system/src/App.js
+++ solar_system/src/App.js
@@ -7,6 +7,10 @@ import './App.css';
 import SolarSystem from './Pages/SolarSystem';
 import { Venus } from './Pages/Venus';
 import { Earth } from './Pages/Earth';
+import { Jupiter } from './Pages/Jupiter';
+import { Uranus } from './Pages/Uranus';
+import { Neptune } from './Pages/Neptune';
+import { Saturn } from './Pages/Saturn';
 
 const App = () => {
   return (
@@ -19,6 +23,10 @@ const App = () => {
             <Route path= "/mercury" exact={true} element={<Mercury />}/>
             <Route path= "/venus" exact={true} element={<Venus />}/>
             <Route path= "/earth" exact={true} element={<Earth/>}/>
+            <Route path= "/jupiter" exact={true} element={<Jupiter/>}/>
+            <Route path= "/uranus" exact={true} element={<Uranus/>}/>
+            <Route path= "/neptune" exact={true} element={<Neptune/>}/>
+            <Route path= "/saturn" exact={true} element={<Saturn/>}/>
           </Routes> 
         </BrowserRouter> 
     </div>
