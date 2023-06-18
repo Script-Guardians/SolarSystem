e7dd6dd279bcfd8f8baa563cc324059c765eefb8
Melissa Schaefer <mschaefer@xpanxion.com>
2023-06-18 19:12:31 -0400

    update with header-footer

--- solar_system/src/Nav.js
+++ solar_system/src/Nav.js
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Nav.css'; // Import the CSS file for styling
-=======
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Nav.css';
@@ -18,30 +13,19 @@ export const Nav = ({ pages }) => {
   const handleClosePopout = () => {
     setIsPopoutOpen(false);
   };
->>>>>>> 9dca86bed8d1b4cd828cbacbfded871f742f7aa1
 
   return (
     <div className="nav-container">
-<<<<<<< HEAD
-      <Link to="/" className="nav-link">Solar System</Link>
-      <Link to="/venus" className="nav-link">Venus</Link>
-      <Link to="/mercury" className="nav-link">Mercury</Link>
-      <Link to="/earth" className="nav-link">Earth</Link>
-      <Link to="/mars" className="nav-link">Mars</Link>
-      <Link to="/jupiter" className="nav-link">Jupiter</Link>
-      <Link to="/saturn" className="nav-link">Saturn</Link>
-      <Link to="/uranus" className="nav-link">Uranus</Link>
-      <Link to="/neptune" className="nav-link">Neptune</Link>
-    </div>
-  );
-}
-=======
       <div className="nav-links">
-        {pages.map((page) => (
-          <Link key={page.path} to={page.path}>
-            {page.name}
-          </Link>
-        ))}
+        <Link to="/" className="nav-link">Solar System</Link>
+        <Link to="/venus" className="nav-link">Venus</Link>
+        <Link to="/mercury" className="nav-link">Mercury</Link>
+        <Link to="/earth" className="nav-link">Earth</Link>
+        <Link to="/mars" className="nav-link">Mars</Link>
+        <Link to="/jupiter" className="nav-link">Jupiter</Link>
+        <Link to="/saturn" className="nav-link">Saturn</Link>
+        <Link to="/uranus" className="nav-link">Uranus</Link>
+        <Link to="/neptune" className="nav-link">Neptune</Link>
       </div>
       <button className="popout-toggle" onClick={handlePopoutToggle}>
         Toggle Popout
@@ -52,5 +36,3 @@ export const Nav = ({ pages }) => {
     </div>
   );
 };
-
->>>>>>> 9dca86bed8d1b4cd828cbacbfded871f742f7aa1
